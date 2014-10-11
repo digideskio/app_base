3653cbe0321baf78023cdd6c2b40befc65760344
Kai Chan Vong <kai@kaivong.com>
2014-10-11 01:56:18 +0100

    Group preview/build configurations together

--- config.rb
+++ config.rb
@@ -2,14 +2,14 @@ require "extensions/dir_manager"
 
 activate :dir_manager
 
-configure :development do
-  activate :livereload
-end
-
 set :haml, { :ugly => true, :format => :html5 }
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
+
+configure :development do
+  activate :livereload
+end
 
 configure :build do
   activate :minify_css
